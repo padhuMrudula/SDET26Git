71fc62ff105e03966403e728eb0b9d77999c819f
dell-pc <dell-pc@DESKTOP-RQ6IQKO>
2021-12-09 14:49:14 +0530

    commit it

---
+++ src/main/java/sdet/SampleProgramGit.java
@@ -0,0 +1,12 @@
+package sdet;
+
+import org.testng.annotations.Test;
+
+public class SampleProgramGit {
+	@Test
+	public void sampleProgramGit() {
+		System.out.println("user1 is commited ");
+		
+	}
+
+}
