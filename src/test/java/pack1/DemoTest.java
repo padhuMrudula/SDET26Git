f274a5a175f49cfc70912612e771b59ee2bfbfba
dell-pc <dell-pc@DESKTOP-RQ6IQKO>
2021-12-13 15:12:17 +0530

    jenkin

--- src/test/java/pack1/DemoTest.java
+++ src/test/java/pack1/DemoTest.java
@@ -14,7 +14,7 @@ public class DemoTest {
 		System.out.println(browser);
 		System.out.println(username);
 		System.out.println(password);
-		System.out.println("hi ");
+		System.out.println("              hi ");
 		
 		
 	}
--- src/test/java/pack1/SampleTest1.java
+++ src/test/java/pack1/SampleTest1.java
@@ -7,7 +7,7 @@ public class SampleTest1 {
 	public void sampleTest1() {
 		System.out.println("test 1 is running");
 		System.out.println("ready to run it");
-		System.out.println(" hii ");
+		System.out.println(" hii            ");
 		
 	}
 
